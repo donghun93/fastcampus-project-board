7abf8a23cab7c10bcc8c574652594cc8897b052b
donghun93 <alswn4516@gmail.com>
2022-07-29 10:56:31 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계했다.
    생성자와 'equals()', 'hashCode()' 등이 도메인 맥락에 맞게끔 디테일하게 설정함

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,12 +1,33 @@
 package com.fastcampus.projectboard.domain;
 
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
+    @ManyToOne(optional = false)  //optional 필수값이다.
     private Article article; // 게시글 (ID)
 
+    @Setter
+    @Column(nullable = false, length = 255)
     private String content; // 본문
 
     private LocalDateTime createdAt; // 생성일시
